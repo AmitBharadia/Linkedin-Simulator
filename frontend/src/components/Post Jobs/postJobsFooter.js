3b4de5487e97999ea51ee8cbb2ec8646011fe63d
Dharma Dheeraj Chintala <38597753+dharmadheeraj@users.noreply.github.com>
2018-11-11 15:44:57 -0800

    Update frontend/src/components/Post Jobs/postJobsFooter.js

---
+++ frontend/src/components/Post Jobs/postJobsFooter.js
@@ -0,0 +1,48 @@
+import React from "react";
+
+class postJobsFooter {
+
+    render()
+    {
+        return(
+            <div>
+                <div className="container">
+                    <div className="row">
+                        <div className="p-3 col-md-8">
+                            <div className="blockquote mb-0">
+                                <p>"I throw myself down among the tall grass by the trickling stream; and, as I lie close to the earth, a thousand unknown plants are noticed by me: when I hear the buzz of the little world among the stalks."</p>
+                                <div className="blockquote-footer">
+                                    <b>J. W. Goethe</b>, CEO at Werther Inc.</div>
+                            </div>
+                        </div>
+                        <div className="col-md-2 mx-auto p-3"> <img className="img-fluid d-block rounded-circle" src="https://static.pingendo.com/img-placeholder-2.svg" /> </div>
+                    </div>
+                </div>
+                <div className="text-center">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-md-12">
+                                <h1>I feel the charm of existence in this spot.</h1>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="container">
+                    <div className="row">
+                        <div className="p-3 col-md-8">
+                            <div className="blockquote mb-0">
+                                <p>"I throw myself down among the tall grass by the trickling stream; and, as I lie close to the earth, a thousand unknown plants are noticed by me: when I hear the buzz of the little world among the stalks."</p>
+                                <div className="blockquote-footer">
+                                    <b>J. W. Goethe</b>, CEO at Werther Inc.</div>
+                            </div>
+                        </div>
+                        <div className="col-md-2 mx-auto p-3"> <img className="img-fluid d-block rounded-circle" src="https://static.pingendo.com/img-placeholder-2.svg" /></div>
+                    </div>
+                </div>
+            </div>
+
+        )
+    }
+}
+
+export default postJobsFooter;
